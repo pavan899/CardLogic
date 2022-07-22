be869bf695b2ffcbebe107a93d1de5fd81a5e82f
pavan899 <pavanteja899@gmail.com>
2022-07-22 23:35:53 +0530

    removed the reference image

--- src/index.js
+++ src/index.js
@@ -149,7 +149,6 @@ const Rummy = () => {
     <div className="mobileModal">
       <Header />
       <Board />
-      <img src='./Assets/UI.svg' style={{ height: '100%', width: 'auto', position: 'absolute', opacity: 0.2, left: 0, top: 0 }} />
       <div className="opencardDeck">
         <CardDeck openCard={openCard} openJoker={openJoker} openCardClicked={openCardClicked} />
       </div>
